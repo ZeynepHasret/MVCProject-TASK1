52eeffdb110b28bd884aeb06cb22109a29e6c8a5
Zeynep HASRET <zeynepphasrett@gmail.com>
2021-04-24 19:01:55 +0300

    full project upload

---
+++ DataAccessLayer/Abstract/ICategoryDAL.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    //OLMAMASI GEREKEN BİR Interface yalnızda Category tablosuna özel olmamalı tüm entity tablolarını kapsamalı.Buyüzden IRepository oluşturuldu
+    public interface ICategoryDAL:IRepository<Category>
+    {
+        //CRUD   interface method oluştururken  Type name();
+
+
+        //List<Category> List();
+        //void Insert(Category p);
+        //void Update(Category p);
+        //void Delete(Category p);
+    }
+}
